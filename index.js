dd08f97d606a80f2451fd18ec6a0095ea0a282aa
Muhammad Umar Nawaz <umarnawaz33@gmail.com>
2021-09-26 16:15:34 +0200

    Complete app.

---
+++ index.js
@@ -0,0 +1,16 @@
+const play = () => {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var img1 = "./images/dice" + randomNumber1 + ".png";
+
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var img2 = "./images/dice" + randomNumber2 + ".png";
+
+    document.querySelector(".img1").setAttribute("src", img1);
+    document.querySelector(".img2").setAttribute("src", img2);
+
+    if (randomNumber1 === randomNumber2) document.querySelector("h1").innerHTML = "Game Draw!";
+    else if (randomNumber1 > randomNumber2) document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    else document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+
+play();
